7f3640dc44fed9fb0f33897f2fbd29480823c9ba
Vladlen Fedorenko <vladlen.fedorenko@gmail.com>
2022-04-29 14:52:05 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
-# first calc data to sort
+################################################################################
+# Sort data setup
+################################################################################
 import random
 
 range_min = 1
-range_max = 15
-elem_count = 20
+range_max = 20
+elem_count = 25
 data_to_sort = []
 
 
@@ -14,7 +16,9 @@ def calc_data():
         data_to_sort.append(random.randint(range_min, range_max))
 
 
-# visualization of bubble sort
+################################################################################
+# Params for sort visualization
+################################################################################
 import pygame
 
 pygame.init()
@@ -22,42 +26,69 @@ pygame.init()
 rect_color = (255, 0, 0)
 text_color = (0, 255, 0)
 
-data_count = elem_count
-max_value = range_max
-
-font_height = 18
-font_values = pygame.font.Font('freesansbold.ttf', font_height)
-font_info = pygame.font.Font('freesansbold.ttf', 18)
-font_sorted = pygame.font.Font('freesansbold.ttf', 32)
+font_values = pygame.font.Font('freesansbold.ttf', 16)
+font_info = pygame.font.Font('freesansbold.ttf', 14)
+font_sorted = pygame.font.Font('freesansbold.ttf', 30)
 
 text = font_values.render(str(range_max), True, text_color)
 textRect = text.get_rect()
-
-
-text_y_offset = font_height / 2
 
 block_width = textRect.width
 block_gap = 5
 block_offset = block_width + block_gap
 one_block_height = block_width
-border_offset = 20
+border_offset = font_info.get_height() + 2
 
-screen_width = data_count * block_offset - block_gap + 2 * border_offset
-screen_height = one_block_height * max_value + 2 * border_offset + font_height
+screen_width = elem_count * block_offset - block_gap + 2 * border_offset
+screen_height = one_block_height * range_max + font_values.get_height() + 2 * border_offset
 
 screen = pygame.display.set_mode([screen_width, screen_height])
 pygame.display.set_caption('Sort view')
 
-is_pause = True
+is_pause = False
 is_sorted = False
 is_show_info = False
-is_demo_enabled = False
+is_demo_enabled = True
 is_sort_from_small_to_greater = True
 
 clock = pygame.time.Clock()
 pygame.time.set_timer(pygame.USEREVENT, 200)
 
 
+################################################################################
+# Wrappers over draw calls
+################################################################################
+def translate_pos(pos):
+    x, y = pos
+    return x, -y + screen_height
+
+
+# pos - left bottom rect corner pos
+def draw_rect(surface, color, pos, width, height):
+    left, top = translate_pos(pos)
+    pygame.draw.rect(surface, color, pygame.Rect(left, top - height, width, height))
+
+
+def draw_circle(surface, color, center, radius):
+    pygame.draw.circle(surface, color, translate_pos(center), radius)
+
+
+def draw_line(surface, color, start, end, width=1):
+    pygame.draw.line(surface, color, translate_pos(start), translate_pos(end), width=width)
+
+
+def draw_text(surface, color, pos, value, font, bgcolor=None, alpha=None):
+    text = font.render(str(value), True, color, bgcolor)
+    textRect = text.get_rect()
+    textRect.center = pos
+    if alpha is not None:
+        text.set_alpha(alpha)
+    surface.blit(text, textRect)
+
+
+################################################################################
+# Draw modes
+################################################################################
 def draw_mode_rect(surface, color, pos, index, value):
     start_x, start_y = pos
     rect_pos = (start_x + block_offset * index, start_y)
@@ -102,8 +133,6 @@ draw_modes = [
 ]
 
 draw_modes_dict = dict(draw_modes)
-print(draw_modes_dict)
-
 draw_mode = 'rect'
 
 
@@ -123,34 +152,9 @@ def next_draw_mode():
     draw_mode = draw_modes[idx][0]
 
 
-def translate_pos(pos):
-    x, y = pos
-    return x, -y + screen_height
-
-
-# pos - left bottom rect corner pos
-def draw_rect(surface, color, pos, width, height):
-    left, top = translate_pos(pos)
-    pygame.draw.rect(surface, color, pygame.Rect(left, top - height, width, height))
-
-
-def draw_circle(surface, color, center, radius):
-    pygame.draw.circle(surface, color, translate_pos(center), radius)
-
-
-def draw_line(surface, color, start, end, width=1):
-    pygame.draw.line(surface, color, translate_pos(start), translate_pos(end), width=width)
-
-
-def draw_text(surface, color, pos, value, font, bgcolor=None, alpha=None):
-    text = font.render(str(value), True, color, bgcolor)
-    textRect = text.get_rect()
-    textRect.center = pos
-    if alpha is not None:
-        text.set_alpha(alpha)
-    surface.blit(text, textRect)
-
-
+################################################################################
+# Main logic functions
+################################################################################
 def draw_elem(surface, color, pos, index, value):
     draw_func = draw_modes_dict[draw_mode]
     draw_func(surface, color, pos, index, value)
@@ -161,7 +165,7 @@ def draw_data(surface, rect_color, text_color, pos, data):
     for n, x in enumerate(data):
         draw_elem(surface, rect_color, pos, n, x)
 
-        text_pos = (start_x + block_offset * n + block_width / 2, start_y + x * one_block_height + text_y_offset)
+        text_pos = (start_x + block_offset * n + block_width / 2, start_y + x * one_block_height + font_values.get_height() / 2)
         draw_text(surface, text_color, translate_pos(text_pos), x, font_values)
 
 
@@ -240,6 +244,9 @@ def set_random_sort_direction():
         is_sort_from_small_to_greater = not is_sort_from_small_to_greater
 
 
+################################################################################
+# Main logic
+################################################################################
 reset()
 
 running = True
@@ -266,6 +273,12 @@ while running:
             elif event.key == pygame.K_TAB:
                 is_show_info = not is_show_info
 
+    # do some updates
+    if is_demo_enabled:
+        if is_sorted is True:
+            set_random_draw_mode()
+            set_random_sort_direction()
+            reset()
 
     # draw black background
     screen.fill((0, 0, 0))
@@ -274,12 +287,6 @@ while running:
     draw_data(screen, rect_color, text_color, (border_offset, border_offset), data_to_sort)
     draw_info_text(screen)
     draw_sorted_text(screen)
-
-    if is_demo_enabled:
-        if is_sorted is True:
-            set_random_draw_mode()
-            set_random_sort_direction()
-            reset()
 
     pygame.display.flip()
     clock.tick(60)
